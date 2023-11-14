4802608ce94baa2634240e4580b9dad73a6bc6a7
ZertLoxe <74061122+ZertLoxe@users.noreply.github.com>
2023-11-14 12:14:00 +0100

    New navigation update!
    -new visual changes on navigation bar for Mobile
    "now if you visit our website on your mobile phone or small tablet a navigation button will appear instead of the navigation bar, clicking on it will show the navigation Buttons."
    *soon the webpages will be suitable for mobile.

--- JS/main.js
+++ JS/main.js
@@ -23,64 +23,77 @@ function scrollIndicatorFunction() {
 }
 
 function navigationScroll() {
-    if (document.body.scrollTop > 85 || document.documentElement.scrollTop > 85) {
-        getElement("header").style.background = "linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0))";
-        getElement("header").style.backdropFilter = "blur(10px)";
-        getElement("header").style.border = "1px solid rgba(255, 255, 255, 0.18)";
-        getElement("header").style.boxShadow = "0 0px 32px 0 rgba(0, 0, 0, 0.37)";
-        getElement("header").style.height = "86px";
-        getElement("main-logo").style.height = "70px";
-        getElement("main-logo").style.marginBlock = "7.5px";
-        getElement("main-logo").style.borderRadius = "10px";
-        getElement("nav-link-1").style.paddingBlock = "15px";
-        getElement("nav-link-2").style.paddingBlock = "15px";
-        getElement("nav-link-3").style.paddingBlock = "15px";
-        getElement("nav-link-active").style.paddingBlock = "15px";
-        getElement("nav-link-1").style.marginBlock = "15.5px";
-        getElement("nav-link-2").style.marginBlock = "15.5px";
-        getElement("nav-link-3").style.marginBlock = "15.5px";
-        getElement("nav-link-active").style.marginBlock = "15.5px";
-        getElement("nav-link-1").style.marginInline = "5px";
-        getElement("nav-link-2").style.marginInline = "5px";
-        getElement("nav-link-3").style.marginInline = "5px";
-        getElement("nav-link-active").style.marginInline = "5px";
-        getElement("nav-link-1").style.borderRadius = "5px";
-        getElement("nav-link-2").style.borderRadius = "5px";
-        getElement("nav-link-3").style.borderRadius = "5px";
-        getElement("nav-link-active").style.borderRadius = "5px";
-        getElement("nav-link-1").style.backgroundColor = "hsla(229, 89%, 4%, 0.75)";
-        getElement("nav-link-2").style.backgroundColor = "hsla(229, 89%, 4%, 0.75)";
-        getElement("nav-link-3").style.backgroundColor = "hsla(229, 89%, 4%, 0.75)";
-        getElement("nav-link-active").style.backgroundColor = "hsla(228, 90%, 16%, 0.75)";
-      }
-    else {
-        getElement("header").style.background = "#010411";
-        getElement("header").style.backdropFilter = "blur(0px)";
-        getElement("header").style.border = "0";
-        getElement("header").style.boxShadow = "0px 0px 10px #010411";
-        getElement("header").style.height = "85px";
-        getElement("main-logo").style.height = "85px";
-        getElement("main-logo").style.marginBlock = "0px";
-        getElement("main-logo").style.borderRadius = "0px";
-        getElement("nav-link-1").style.paddingBlock = "30.5px";
-        getElement("nav-link-2").style.paddingBlock = "30.5px";
-        getElement("nav-link-3").style.paddingBlock = "30.5px";
-        getElement("nav-link-active").style.paddingBlock = "30.5px";
-        getElement("nav-link-1").style.marginBlock = "0px";
-        getElement("nav-link-2").style.marginBlock = "0px";
-        getElement("nav-link-3").style.marginBlock = "0px";
-        getElement("nav-link-active").style.marginBlock = "0px";
-        getElement("nav-link-1").style.marginInline = "0px";
-        getElement("nav-link-2").style.marginInline = "0px";
-        getElement("nav-link-3").style.marginInline = "0px";
-        getElement("nav-link-active").style.marginInline = "0px";
-        getElement("nav-link-1").style.borderRadius = "0px";
-        getElement("nav-link-2").style.borderRadius = "0px";
-        getElement("nav-link-3").style.borderRadius = "0px";
-        getElement("nav-link-active").style.borderRadius = "0px";
-        getElement("nav-link-1").style.backgroundColor = "hsla(229, 89%, 4%, 1)";
-        getElement("nav-link-2").style.backgroundColor = "hsla(229, 89%, 4%, 1)";
-        getElement("nav-link-3").style.backgroundColor = "hsla(229, 89%, 4%, 1)";
-        getElement("nav-link-active").style.backgroundColor = "hsla(228, 90%, 16%, 1)";
-      }
-}
+    if (window.innerWidth > 740) {
+        console.log('Larger than Small Media Query Matched!');
+        if (document.body.scrollTop > 85 || document.documentElement.scrollTop > 85) {
+            getElement("header").style.background = "linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0))";
+            getElement("header").style.backdropFilter = "blur(10px)";
+            getElement("header").style.border = "1px solid rgba(255, 255, 255, 0.18)";
+            getElement("header").style.boxShadow = "0 0px 32px 0 rgba(0, 0, 0, 0.37)";
+            getElement("header").style.height = "86px";
+            getElement("main-logo").style.height = "70px";
+            getElement("main-logo").style.marginBlock = "7.5px";
+            getElement("main-logo").style.borderRadius = "10px";
+            getElement("nav-link-1").style.paddingBlock = "15px";
+            getElement("nav-link-2").style.paddingBlock = "15px";
+            getElement("nav-link-3").style.paddingBlock = "15px";
+            getElement("nav-link-active").style.paddingBlock = "15px";
+            getElement("nav-link-1").style.marginBlock = "15.5px";
+            getElement("nav-link-2").style.marginBlock = "15.5px";
+            getElement("nav-link-3").style.marginBlock = "15.5px";
+            getElement("nav-link-active").style.marginBlock = "15.5px";
+            getElement("nav-link-1").style.marginInline = "5px";
+            getElement("nav-link-2").style.marginInline = "5px";
+            getElement("nav-link-3").style.marginInline = "5px";
+            getElement("nav-link-active").style.marginInline = "5px";
+            getElement("nav-link-1").style.borderRadius = "5px";
+            getElement("nav-link-2").style.borderRadius = "5px";
+            getElement("nav-link-3").style.borderRadius = "5px";
+            getElement("nav-link-active").style.borderRadius = "5px";
+            getElement("nav-link-1").style.backgroundColor = "hsla(229, 89%, 4%, 0.75)";
+            getElement("nav-link-2").style.backgroundColor = "hsla(229, 89%, 4%, 0.75)";
+            getElement("nav-link-3").style.backgroundColor = "hsla(229, 89%, 4%, 0.75)";
+            getElement("nav-link-active").style.backgroundColor = "hsla(228, 90%, 16%, 0.75)";
+        }
+        else {
+            getElement("header").style.background = "#010411";
+            getElement("header").style.backdropFilter = "blur(0px)";
+            getElement("header").style.border = "0";
+            getElement("header").style.boxShadow = "0px 0px 10px #010411";
+            getElement("header").style.height = "85px";
+            getElement("main-logo").style.height = "85px";
+            getElement("main-logo").style.marginBlock = "0px";
+            getElement("main-logo").style.borderRadius = "0px";
+            getElement("nav-link-1").style.paddingBlock = "30.5px";
+            getElement("nav-link-2").style.paddingBlock = "30.5px";
+            getElement("nav-link-3").style.paddingBlock = "30.5px";
+            getElement("nav-link-active").style.paddingBlock = "30.5px";
+            getElement("nav-link-1").style.marginBlock = "0px";
+            getElement("nav-link-2").style.marginBlock = "0px";
+            getElement("nav-link-3").style.marginBlock = "0px";
+            getElement("nav-link-active").style.marginBlock = "0px";
+            getElement("nav-link-1").style.marginInline = "0px";
+            getElement("nav-link-2").style.marginInline = "0px";
+            getElement("nav-link-3").style.marginInline = "0px";
+            getElement("nav-link-active").style.marginInline = "0px";
+            getElement("nav-link-1").style.borderRadius = "0px";
+            getElement("nav-link-2").style.borderRadius = "0px";
+            getElement("nav-link-3").style.borderRadius = "0px";
+            getElement("nav-link-active").style.borderRadius = "0px";
+            getElement("nav-link-1").style.backgroundColor = "hsla(229, 89%, 4%, 1)";
+            getElement("nav-link-2").style.backgroundColor = "hsla(229, 89%, 4%, 1)";
+            getElement("nav-link-3").style.backgroundColor = "hsla(229, 89%, 4%, 1)";
+            getElement("nav-link-active").style.backgroundColor = "hsla(228, 90%, 16%, 1)";
+        }
+    }
+};
+
+var navPosition = 0;
+function navPositionAction() {
+    navPosition = 1 - navPosition;
+    if (navPosition === 1) {
+        document.getElementById('nav-links').style.top = '65vh';
+    } else {
+        document.getElementById('nav-links').style.top = '100vh';
+    }
+};
